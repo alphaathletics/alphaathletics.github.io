9473430c01f08da3f4ba86f4119861ee005c4e3a
Tree Man <jammin.irving@gmail.com>
2014-06-14 12:03:36 -0700

    boom

--- js/index.js
+++ js/index.js
@@ -1,21 +1,33 @@
 
-$('#home').hover(function(){
+$('#home').hover(function()
+{
 	$('#home').css( "background-color" , "#009080" );
-},function(){
-	$('#home').css( "background-color", "#0080B0" );
+	$('#home').css( "border-color" , "black" );
+}
+,function()
+{
+	$('#home').css( "background-color", "#661100" );
+	$('#home').css( "border-color", "white" );
+
 });
+
+
+
+
+
+
 $('#photos').hover(function(){
 	$('#photos').css( "background-color" , "#009080" );
 },function(){
-	$('#photos').css( "background-color", "#0080B0" );
+	$('#photos').css( "background-color", "#661100" );
 });
 $('#videos').hover(function(){
 	$('#videos').css( "background-color" , "#009080" );
 },function(){
-	$('#videos').css( "background-color", "#0080B0" );
+	$('#videos').css( "background-color", "#661100" );
 });
 $('#about').hover(function(){
 	$('#about').css( "background-color" , "#009080" );
 },function(){
-	$('#about').css( "background-color", "#0080B0" );
+	$('#about').css( "background-color", "#661100" );
 });
